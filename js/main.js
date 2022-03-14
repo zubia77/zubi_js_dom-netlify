26845e6e060d8004e4fdb18df145b9a71da38f0c
Zubia <rashzubia@gmail.com>
2022-03-14 13:08:02 +0100

    data added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-import { capitalizeFirstCharacter, getTodaysDate } from './tools.js';
+import * as tools from './tools.js';
+import { flashcards } from './data/flashcards.js';
 
 const contentElem = document.querySelector('.content');
 
 contentElem.innerHTML = `
-<p>${capitalizeFirstCharacter('this is a test')}</p>
-<p>${getTodaysDate()}`;
+<p>${tools.getTodaysDate()}</p>
+<div class="flashcards">
+	${flashcards.map(flashcard => {
+	return `
+	<div class="flashcard">
+		<div class="front"><span class="category">${flashcard.category.toUpperCase()}</span>: ${tools.capitalizeFirstCharacter(flashcard.front)}</div>
+		<div class="back">${flashcard.back}</div>	
+	</div>	
+		`;
+}).join('')}
+</div>
+`;
